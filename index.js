cf4ef674bd97376c7d6b435381a6bfa7b19adfce
Harry Khanna <harry@getmagistrate.com>
2023-02-08 09:23:05 -0500

    Token authentication

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+const authentication = {
+  type: 'custom',
+  test: {
+    url: 'https://api.staging.getmagistrate.com/v1/me/',
+  },
+  fields: [
+    {
+      key: 'api_key',
+      type: 'string',
+      required: true,
+      helpText: 'Found on your settings page.',
+    },
+  ],
+};
+
+const addApiKeyToHeader = (request, z, bundle) => {
+  request.headers.Authorization = `Token ${bundle.authData.apiKey}`;
+  return request;
+};
+
+
 module.exports = {
   // This is just shorthand to reference the installed dependencies you have.
   // Zapier will need to know these before we can upload.
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
+  authentication,
+  beforeRequest: [addApiKeyToHeader],
 
   // If you want your trigger to show up, you better include it here!
   triggers: {},
---
+++ test/authentication.test.js
@@ -0,0 +1,38 @@
+/* globals describe, it, expect */
+
+const zapier = require('zapier-platform-core');
+
+const App = require('../index');
+const appTester = zapier.createAppTester(App);
+
+describe('custom auth', () => {
+  it('passes authentication and returns json', async () => {
+    zapier.tools.env.inject()
+    const bundle = {
+      authData: {
+        apiKey: process.env.API_KEY,
+      },
+    };
+
+    const response = await appTester(App.authentication.test, bundle);
+    expect(response).toHaveProperty('uuid');
+    expect(response).toHaveProperty('name');
+    expect(response).toHaveProperty('email');
+  });
+
+  it('fails on bad auth', async () => {
+    const bundle = {
+      authData: {
+        apiKey: 'bad',
+      },
+    };
+
+    try {
+      await appTester(App.authentication.test, bundle);
+    } catch (error) {
+      expect(error.message).toContain('Invalid token');
+      return;
+    }
+    throw new Error('appTester should have thrown');
+  });
+});
