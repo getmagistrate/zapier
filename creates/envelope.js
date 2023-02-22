d93a19ab358336b2ec55c5e8cd5bbe817e466e53
Harry Khanna <harry@getmagistrate.com>
2023-02-22 13:37:06 -0500

    Refactor create utils

--- creates/envelope.js
+++ creates/envelope.js
@@ -1,27 +1,5 @@
 const commonInputFields = require("./commonInputFields");
-
-const unflatten = (obj, skipFirstKey) => {
-  // Given the flat nature of Zapier keys, turn it into
-  // a nested object Magistrate's API consumes.
-  const result = {};
-  for (const [key, value] of Object.entries(obj)) {
-    const keys = key.split(".");
-    let nested = result;
-    const start = skipFirstKey ? 1 : 0;
-    for (let i = start; i < keys.length - 1; i++) {
-      const k = keys[i];
-      nested[k] = nested[k] || {};
-      nested = nested[k];
-    }
-    const lastKey = keys[keys.length - 1];
-    if (Array.isArray(value)) {
-      nested[lastKey] = value.map((item) => unflatten(item, true));
-    } else {
-      nested[lastKey] = value;
-    }
-  }
-  return result;
-};
+const { unflatten } = require("./utils");
 
 const perform = async (z, bundle) => {
   const body = unflatten(bundle.inputData);
---
+++ creates/utils.js
@@ -0,0 +1,26 @@
+const unflatten = (obj, skipFirstKey) => {
+  // Given the flat nature of Zapier keys, turn it into
+  // a nested object Magistrate's API consumes.
+  const result = {};
+  for (const [key, value] of Object.entries(obj)) {
+    const keys = key.split(".");
+    let nested = result;
+    const start = skipFirstKey ? 1 : 0;
+    for (let i = start; i < keys.length - 1; i++) {
+      const k = keys[i];
+      nested[k] = nested[k] || {};
+      nested = nested[k];
+    }
+    const lastKey = keys[keys.length - 1];
+    if (Array.isArray(value)) {
+      nested[lastKey] = value.map((item) => unflatten(item, true));
+    } else {
+      nested[lastKey] = value;
+    }
+  }
+  return result;
+};
+
+module.exports = {
+  unflatten,
+};
